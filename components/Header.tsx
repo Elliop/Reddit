d64d8e9e980c8535587e239bc31839ae8d985fa4
Abdelmouhaymine Salah <abdelmouhayminegx@gmail.com>
2022-05-19 15:06:52 +0100

    add left section of the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,13 +4,13 @@ import { ChevronDownIcon, HomeIcon } from '@heroicons/react/solid'
 
 const Header = () => {
   return (
-    <div>
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
         <Image objectFit="contain" src="/Reddit_logo.png" layout="fill" />
       </div>
-      <div className="flex items-center">
+      <div className="mx-7 flex items-center xl:min-w-[300px]">
         <HomeIcon className="h-5 w-5" />
-        <p>Home</p>
+        <p className="ml-2 hidden flex-1 lg:inline">Home</p>
         <ChevronDownIcon className="h-5 w-5" />
       </div>
     </div>
