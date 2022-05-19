2d9f274a653f3b9f0601a6c45ffcc420e681884a
Abdelmouhaymine Salah <abdelmouhayminegx@gmail.com>
2022-05-19 15:01:01 +0100

    add header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
+import { ChevronDownIcon, HomeIcon } from '@heroicons/react/solid'
 
 const Header = () => {
   return (
     <div>
-      {/*  */}
-      {/*  */}
-      Header
+      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
+        <Image objectFit="contain" src="/Reddit_logo.png" layout="fill" />
+      </div>
+      <div className="flex items-center">
+        <HomeIcon className="h-5 w-5" />
+        <p>Home</p>
+        <ChevronDownIcon className="h-5 w-5" />
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
     <div className="">
       <Head>
         <title>Reddit Clone</title>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
       <h1>Hello Reddit</h1>
     </div>
   )
