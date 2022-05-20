554d4695d4f277400f669b036269bdf6a2f542de
Abdelmouhaymine Salah <abdelmouhayminegx@gmail.com>
2022-05-20 01:02:52 +0100

    move the header coponent to all the app

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,6 @@ const Home: NextPage = () => {
         <title>Reddit Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
       <h1>Hello Reddit</h1>
     </div>
   )
