c480e3683ced3794408db0ada587757a1846f0fd
Abdelmouhaymine Salah <abdelmouhayminegx@gmail.com>
2022-05-23 16:22:50 +0100

    Building the Body Section of the Post Box

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,6 +1,6 @@
 import { LinkIcon, PhotographIcon } from '@heroicons/react/outline'
 import { useSession } from 'next-auth/react'
-import React from 'react'
+import React, { useState } from 'react'
 import Avatar from './Avatar'
 import { useForm } from 'react-hook-form'
 
@@ -19,6 +19,7 @@ const PostBox = () => {
     watch,
     formState: { errors },
   } = useForm<Inputs>()
+  const [imageBoxOpen, setImageBoxOpen] = useState<boolean>(false)
 
   return (
     <form className="stocky top-16 z-50 rounded-md border border-gray-300 bg-white p-2">
@@ -33,10 +34,50 @@ const PostBox = () => {
             session ? 'Create a post by entring a title!' : 'Sign in to post!'
           }
         />
-        <PhotographIcon className={`h-6 cursor-pointer text-gray-300`} />
-        <LinkIcon className={`h-6 cursor-pointer text-gray-300`} />
+        <PhotographIcon
+          onClick={() => setImageBoxOpen(!imageBoxOpen)}
+          className={`h-6 cursor-pointer text-gray-300 ${
+            imageBoxOpen && 'text-blue-300'
+          }`}
+        />
+        <LinkIcon className="h-6 cursor-pointer text-gray-300" />
       </div>
-      {!!watch('postTitle') && <div>xx</div>}
+      {!!watch('postTitle') && (
+        <div className="flex flex-col py-2">
+          {/* Body */}
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Body:</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register('postBody')}
+              type="text"
+              placeholder="Text (optional)"
+            />
+          </div>
+          {/* subreddit */}
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Subreddit:</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register('subreddit')}
+              type="text"
+              placeholder="i.e reactjs"
+            />
+          </div>
+          {/* Image Box */}
+          {imageBoxOpen && (
+            <div className="flex items-center px-2">
+              <p className="min-w-[90px]">Image URL:</p>
+              <input
+                className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+                {...register('postImage')}
+                type="text"
+                placeholder="Optional..."
+              />
+            </div>
+          )}
+        </div>
+      )}
     </form>
   )
 }
