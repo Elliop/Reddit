69e8d6fb28277f7b0235c0045ae7942da669bd85
Abdelmouhaymine Salah <abdelmouhayminegx@gmail.com>
2022-05-23 13:37:32 +0100

    Building the Avatar Component

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,12 +1,13 @@
 import { useSession } from 'next-auth/react'
 import React from 'react'
+import Avatar from './Avatar'
 
 const PostBox = () => {
   const { data: session } = useSession()
   return (
     <form>
       <div className="flex items-center space-x-3">
-        {/* Avatar */}
+        <Avatar />
         <input
           disabled={!session}
           className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
